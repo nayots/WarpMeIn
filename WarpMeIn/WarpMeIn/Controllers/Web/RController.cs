a32a935069a31a176b4f0183fff3fb92a274940f
Stoyan Grigorov <grigorov.stoyan@gmail.com>
2017-07-04 01:36:34 +0300

    Added basic controllers and sample views

---
+++ WarpMeIn/WarpMeIn/Controllers/Web/RController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WarpMeIn.Controllers.Web
+{
+    [Route("r")]
+    public class RController : Controller
+    {
+        [HttpGet("{id}")]
+        public IActionResult RedirectTo(string id)
+        {
+            return View();
+        }
+    }
+}
