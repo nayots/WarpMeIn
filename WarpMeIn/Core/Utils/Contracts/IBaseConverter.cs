6d6b5c178c023cfa08a63c1aa489877243682388
Stoyan Grigorov <grigorov.stoyan@gmail.com>
2018-01-11 00:23:31 +0200

    clean up

--- WarpMeIn/Core/Utils/Contracts/IBaseConverter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WarpMeIn.Core.Utils.Contracts
-{
-    public interface IBaseConverter
-    {
-        long FromBase(string input, string baseChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
-        string ToBase(long input, string baseChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", int desiredLength = 5);
-    }
-}
